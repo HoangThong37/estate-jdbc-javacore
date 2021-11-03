27e839df8cffd388d980fa5ce753abaff9174223
HoangThong37 <thonghoang115@gmail.com>
2021-11-03 23:01:35 +0700

    constant

--- src/main/java/constant/SystemConstant.java
+++ src/main/java/constant/SystemConstant.java
@@ -1,7 +1,7 @@
 package constant;
 
 public class SystemConstant {
-	public static final String ROLE_MANAGER = "manager";
+	public static final String TANGTRET_CODE = "TANG_TRET";
 	public static final String ROLE_STAFF = "staff";
 	
 
